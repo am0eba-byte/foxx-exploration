2595cd69244f8bbf76ed0994db8da81f3f963f39
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2022-01-19 11:47:46 -0500

    trying stuff out with album collections

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const router = createRouter();
 const joi = require('joi'); // imported from npm
 const db = require(`@arangodb`).db;
 const foxxColl = db._collection(`myFoxxCollection`);
+const albumColl = db._collection('albums')
 const aql = require('@arangodb').aql;
 
 module.context.use(router);
+
+// Getting-Started tutorial
 
 //Basic Hello World route
 router.get('/hello-world', function (req, res){
@@ -50,3 +53,33 @@ router.get('/entries', function(req, res){
 ).required(), 'List of Entry Keys.')
 .summary('List of entry keys.')
 .description('Assembles a list entry keys from myFoxxCollection.')
+
+
+
+
+//Albums Collection Routes
+
+// Add album to albumCollection
+router.post('/albums', function(req, res){
+    const data = req.body;
+    const meta = albumColl.save(req.body);
+    res.send(Object.assign(data, meta));
+})
+.body(joi.object().required(), 'Album to store in the collection.')
+.response(joi.object().required(), 'Album stored in the collection.')
+.summary('Store an album')
+.description('Stores an album in "albums" collection.')
+
+// Retrieve entry from albumCollection using AQL
+router.get('/albums', function(req, res){
+    const keys = db._query(aql`
+    FOR entry IN ${albumColl}
+    RETURN entry.album.name
+    `);
+    res.send(keys);
+})
+.response(joi.array().items(
+    joi.string().required()
+).required(), 'List of album names.')
+.summary('List album names')
+.description('Assembles a list of album names from the "albums" collection.')
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,12 @@
 `use strict`;
 const db = require(`@arangodb`).db;
 const collectionName = `myFoxxCollection`;
+const albumCollection = 'albums';
 
 if(!db._collection(collectionName)){
     db._createDocumentCollection(collectionName);
+}
+
+if(!db._collection(albumCollection)){
+    db._createDocumentCollection(albumCollection);
 }
